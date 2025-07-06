ae7e83cb916acba99012b2f98d1728d6ffdaacec
saitohiroki <hirokisaito0813@gmail.com>
2025-07-06 11:39:19 +0900

    add supabase setting

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "picsum.photos",
       },
+      {
+        protocol: "https",
+        hostname: "trsohkknsxqqkqfhroyk.supabase.co",
+      },
     ],
   },
   experimental: {
@@ -15,6 +19,7 @@ const nextConfig: NextConfig = {
       bodySizeLimit: "5mb",
     },
   },
+  transpilePackages: ["@supabase/supabase-js"],
 };
 
 export default nextConfig;
